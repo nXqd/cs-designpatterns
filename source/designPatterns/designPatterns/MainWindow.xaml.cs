d82a5c18ae66ef960d02b32a461972158df25dc6
nXqd <nxqd.inbox@gmail.com>
2011-12-22 15:15:04 +0700

    create simple mvvm structure

---
+++ source/designPatterns/designPatterns/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace designPatterns
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FacadeBtnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ source/designPatterns/designPatterns/view/WindowView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using designPatterns.model;
+using designPatterns.viewmodel;
+
+namespace designPatterns.view
+{
+    /// <summary>
+    /// Interaction logic for WindowView.xaml
+    /// </summary>
+    public partial class WindowView
+    {
+        public WindowView()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var model = new FacadeWindowModel();
+            var viewModel = new WindowViewModel(model);
+            DataContext = viewModel;
+        }
+
+    }
+}
